8327073c0b4c30403946416f9750fea9f09be075
Deepak Paliwal <deepak.paliwal@gmail.com>
2026-02-11 14:57:05 -0500

    Upgrade springdoc to fix Swagger NoSuchMethodError

---
+++ uums-batch/src/test/java/com/uums/batch/UumsBatchApplicationTests.java
@@ -0,0 +1,12 @@
+package com.uums.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UumsBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
