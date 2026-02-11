0004e262e5e501b99fef89bbf0ab32ece5f41398
Deepak Paliwal <deepak.paliwal@gmail.com>
2026-02-11 15:31:32 -0500

    Fix login role parsing and mask sensitive values in REST logs

---
+++ uums-batch/src/main/java/com/uums/batch/UumsBatchApplication.java
@@ -0,0 +1,12 @@
+package com.uums.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UumsBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UumsBatchApplication.class, args);
+    }
+}
